89ba481e61b584ce74ad77a1e8322c48cabf6c5e
Sam <s@sammagee.me>
2020-11-19 17:22:21 -0600

    updates work detail page

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -30,24 +30,28 @@ export default function Details({ data }) {
           {data.title}
         </h1>
 
-        <div className="flex items-center justify-center mt-2 space-x-6">
+        <div className="flex items-center justify-center mt-2 space-x-4">
           <div className="flex items-center">
             <svg className="w-4 h-4 text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
             </svg>
 
             <Date
-              className="block mt-1 ml-1 text-gray-400"
+              className="block mt-1 ml-1 text-gray-500"
               dateString={data.date}
             />
           </div>
 
-          <a href={`//${data.link}`} className="flex items-center text-gray-400 transition duration-200 ease-in-out hover:text-white focus:text-white" target="_blank" rel="noopener noreferrer">
+          <a href={`//${data.link}`} className="flex items-center text-gray-500 transition duration-200 ease-in-out hover:text-gray-300 focus:text-gray-300 group" target="_blank group" rel="noopener noreferrer">
             <svg className="w-4 h-4 text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clipRule="evenodd" />
             </svg>
 
-            <span className="mt-1 ml-1">{data.link}</span>
+            <span className="relative mt-1 ml-1">
+              {data.link}
+
+              <span className="absolute bottom-0 left-0 inline-block w-full h-0.5 bg-gray-700 transition duration-200 ease-in-out group-hover:bg-gray-500 group-focus:bg-gray-500" />
+            </span>
           </a>
         </div>
 
